f390d15df9b323b26519835d6930d0530157610b
zhouq <zhouq@mayocase.com>
2016-09-18 15:19:31 +0800

    update pom.xml

--- oauth2/database-service/src/main/java/jo/javaee/oauth2/dbservice/DataApplication.java
+++
@@ -1,7 +0,0 @@
-package jo.javaee.oauth2.dbservice;
-
-import javax.ws.rs.core.Application;
-import javax.ws.rs.ApplicationPath;
-@ApplicationPath("/")
-public class DataApplication extends Application {
-}
